e8df95bae04fc88904d017fa26eacde5ea5a603e
bengris32 <bengris32@protonmail.ch>
2024-02-21 19:57:17 +0000

    emerald : Switch to MediaTek health AIDL implementation
    Change-Id: I81511392c9ecc43f879bb4716bade53611425883
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -149,10 +149,9 @@ PRODUCT_PACKAGES += \
     
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-    
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor \
