1d66c53df16d822d09a3da15529e1c85f3b0ca77
Jooyung Han <jooyung@google.com>
2024-03-22 13:38:49 +0000

    emerald : Install bootcontrol HAL APEX
    Bug: 297467553
    Test: VtsHalBootAidlTargetTest
    Change-Id: Id206966e9f181283082b85dadbf409e3bf4017db

--- device.mk
+++ device.mk
@@ -97,7 +97,7 @@ PRODUCT_COPY_FILES += \
     
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.default \
+    com.android.hardware.boot \
     android.hardware.boot-service.default_recovery
     
 PRODUCT_PACKAGES += \
